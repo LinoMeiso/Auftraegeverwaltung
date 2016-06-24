feaa4e7f4b4ffa4ebe5ffa3006158e1805eb07ff
Linus Meiser <linsu.meiser@web.de>
2016-06-24 15:19:45 +0200

    Added Project: Orgadata Aufträge Verwaltung

---
+++ AufträgeOrgadata/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace AufträgeOrgadata
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Program_Loaded(object sender, RoutedEventArgs e)
+        {
+            ProgrammName programme = new ProgrammName();
+            
+            for (int i = 0; i < programme.ProgrammListe.Count; i++)
+            {
+                CheckBox cb = new CheckBox();
+                cb.Width = 115;
+                cb.Height = 15;
+                cb.VerticalAlignment = VerticalAlignment.Top;
+                cb.HorizontalAlignment = HorizontalAlignment.Left;
+                cb.Content = programme.ProgrammListe[i].Name;
+                stackPanelPrograms.Children.Add(cb);
+            }
+
+            Installationsart installationsart = new Installationsart();
+
+            for (int i = 0; i < installationsart.Installationsliste.Count; i++)
+            {
+                CheckBox cb = new CheckBox();
+                cb.Width = 200;
+                cb.Height = 15;
+                cb.VerticalAlignment = VerticalAlignment.Top;
+                cb.HorizontalAlignment = HorizontalAlignment.Left;
+                cb.Content = installationsart.Installationsliste[i].Installationsart;
+                stackPanelInstallation.Children.Add(cb);
+            }
+
+            StammName stamm = new StammName();
+
+            for (int i = 0; i < stamm.StammListe.Count; i++)
+            {
+                CheckBox cb = new CheckBox();
+                cb.Width = 200;
+                cb.Height = 15;
+                cb.VerticalAlignment = VerticalAlignment.Top;
+                cb.HorizontalAlignment = HorizontalAlignment.Left;
+                cb.Content = stamm.StammListe[i].StammName;
+                wpanelStamm.Children.Add(cb);
+            }
+        }
+
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            //Datum und Zeit Ausgabe
+            DateTime date = DateTime.Now;
+            string date1 = date.ToString();
+            string dateOnly = date1.Substring(0, 10);
+            string timeOnly = DateTime.Now.ToShortTimeString();
+
+            lblDate.Content = dateOnly;
+            lblTime.Content = timeOnly;
+        }
+
+        private void txtGrund_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txtGrund.Text.Length > 0)
+                cbGrund.IsChecked = true;
+            else
+                cbGrund.IsChecked = false;
+        }
+
+        private void txtAustausch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txtAustausch.Text.Length > 0)
+                cbAustausch.IsChecked = true;
+            else
+                cbAustausch.IsChecked = false;
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ AufträgeOrgadata/ProgrammName.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Windows;
+
+namespace AufträgeOrgadata
+{
+    public class TProgramm
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class ProgrammName
+    {
+        public List<TProgramm> ProgrammListe { get; set; }
+
+        public ProgrammName()
+        {
+            ProgrammListe = new List<TProgramm>();
+            LoadProgramms();
+        }
+
+        public void LoadProgramms()
+        {
+            String connstring = "uid=root;" + "password=;" + "server=localhost;" + "database=auftraege";
+            MySqlConnection conn = new MySqlConnection(connstring);
+
+            try
+            {
+                conn.Open();
+
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM programm");
+                cmd.Connection = conn;
+
+                using (MySqlDataReader Reader = cmd.ExecuteReader())
+                {
+                    while (Reader.Read())
+                    {
+                        TProgramm programm = new TProgramm();
+                        programm.ID = int.Parse(Reader["ID"].ToString());
+                        programm.Name = Reader["ProgrammName"].ToString();
+                        ProgrammListe.Add(programm);
+                    }
+                }
+                conn.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+
+
+        }
+
+    }
+    public class TInstallationsart
+    {
+        public int ID { get; set; }
+        public string Installationsart { get; set; }
+    }
+
+    public class Installationsart
+    {
+        public List<TInstallationsart> Installationsliste { get; set; }
+
+        public Installationsart()
+        {
+            Installationsliste = new List<TInstallationsart>();
+            LoadProgramms();
+        }
+
+        public void LoadProgramms()
+        {
+            String connstring = "uid=root;" + "password=;" + "server=localhost;" + "database=auftraege";
+            MySqlConnection conn = new MySqlConnection(connstring);
+
+            try
+            {
+                conn.Open();
+
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM Installationsart");
+                cmd.Connection = conn;
+
+                using (MySqlDataReader Reader = cmd.ExecuteReader())
+                {
+                    while (Reader.Read())
+                    {
+                        TInstallationsart installationsart = new TInstallationsart();
+                        installationsart.ID = int.Parse(Reader["ID"].ToString());
+                        installationsart.Installationsart = Reader["Installationsart"].ToString();
+                        Installationsliste.Add(installationsart);
+                    }
+                }
+                conn.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+
+
+        }
+    }
+
+    public class TStamm
+    {
+        public int ID { get; set; }
+        public string StammName { get; set; }
+    }
+
+    public class StammName
+    {
+        public List<TStamm> StammListe { get; set; }
+
+        public StammName()
+        {
+            StammListe = new List<TStamm>();
+            LoadStammdaten();
+        }
+
+        public void LoadStammdaten()
+        {
+            String connstring = "uid=root;" + "password=;" + "server=localhost;" + "database=auftraege";
+            MySqlConnection conn = new MySqlConnection(connstring);
+
+            try
+            {
+                conn.Open();
+
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM stammdaten");
+                cmd.Connection = conn;
+
+                using (MySqlDataReader Reader = cmd.ExecuteReader())
+                {
+                    while (Reader.Read())
+                    {
+                        TStamm stamm = new TStamm();
+                        stamm.ID = int.Parse(Reader["ID"].ToString());
+                        stamm.StammName = Reader["StammName"].ToString();
+                        StammListe.Add(stamm);
+                    }
+                }
+                conn.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+
+
+        }
+
+    }
+}
