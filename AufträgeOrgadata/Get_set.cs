680903dff484833e8b3dd5722e4ac734b4ae4f4a
Dennis Jongebloed <jongebloed.dennis@googlemail.com>
2016-06-30 08:43:32 +0200

    - #Update 08:43

---
+++ AufträgeOrgadata/Get_set.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AufträgeOrgadata
+{
+    class Get_set
+    {
+        public class TKundeAdresse
+        {
+            public string name { get; set; }
+            public string land { get; set; }
+            public string ort { get; set; }
+            public string plz { get; set; }
+            public string ansprechpartner { get; set; }
+        }
+    }
+}
